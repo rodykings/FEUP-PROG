d29940618d388c7c17b4b0897474b7e5bc962b40
Rodrigo Reis <rodrigocreis.12@gmail.com>
2019-02-23 16:30:45 +0000

    Update

---
+++ TP/ex1.1.cpp
@@ -0,0 +1,17 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	char l;
+
+	cout << "Digite um caracter: ";
+	cin >> l;
+	cout <<static_cast<int>(l);
+
+	return 0;
+}
+
+*/
---
+++ TP/ex1.4.cpp
@@ -0,0 +1,33 @@
+/*#include <iostream>;
+
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e, f;
+	int x, y;
+
+	cout << "a ? ";
+	cin >> a;
+	cout << "b ? ";
+	cin >> b;
+	cout << "c ? ";
+	cin >> c;
+	cout << "d ? ";
+	cin >> d;
+	cout << "e ? ";
+	cin >> e;
+	cout << "f ? ";
+	cin >> f;
+
+	//x = (c·e - b·f) / (a·e - b·d) 
+	x = (c*e - b * f) / (a*e - b * d);
+
+	//y = (a·f - c·d) / (a·e - b·d)
+	y = (a*f - c*d) / (a*e - b*d);
+
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+
+	return 0;
+}*/
